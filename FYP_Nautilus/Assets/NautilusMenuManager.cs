992f281f8ec2ee84b23270db5f86a976207c9189
ZnamR3B <znamr3b@gmail.com>
2021-08-12 15:27:07 +0800

    update

--- FYP_Nautilus/Assets/NautilusMenuManager.cs
+++ FYP_Nautilus/Assets/NautilusMenuManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NautilusMenuManager : MonoBehaviour
 {
     GameObject playerObject;
+    PlayerInteractionController playerInteractionController;
 
     public GameObject mainMenu;
     public GameObject oceanMap;
@@ -17,10 +18,22 @@ public class NautilusMenuManager : MonoBehaviour
 
     int mapIndex; // 0 = ronda
     bool inMenu;
+    bool inSubMenu;
 
-    
+    private void Update()
+    {
+        if(inMenu)
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                closeMenu();
+            }
+        }
+    }
     public void openOceanMap()
     {
+        inSubMenu = true;
+        inMenu = false;
         nautilusOceanManager.openMenu(mapIndex, playerInfo);
         mainMenu.SetActive(false);
         teamMemberMap.SetActive(false);
@@ -28,13 +41,22 @@ public class NautilusMenuManager : MonoBehaviour
 
     public void openMemberChangeMenu()
     {
-
+        inSubMenu = true;
+        inMenu = false;
     }
 
     public void openMainMenu()
     {
+        //menu on
+        inMenu = true;
+        inSubMenu = false;
+        //locked cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        //get player info
         if (playerInfo == null)
             playerInfo = FindObjectOfType<PlayerInfo>();
+
         mapIndex = playerInfo.mapIndex;
         mainMenu.SetActive(true);
     }
@@ -43,14 +65,24 @@ public class NautilusMenuManager : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerObject = other.gameObject;
-            other.GetComponent<PlayerInteractionController>().setPlayerLock(true);
+            playerInteractionController = other.GetComponent<PlayerInteractionController>();
+            playerInteractionController.setPlayerLock(true);
+            playerInteractionController.inInteraction = true;
             openMainMenu();
         }
     }
 
     public void closeMenu()
     {
+        //menu off
         mainMenu.SetActive(false);
+        //player interaction setting
+        playerInteractionController.setPlayerLock(false);
+        playerInteractionController.inInteraction = false;
+        //unlock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        //set player pos
         playerObject.transform.position = exit.position;
         playerObject.transform.localRotation = exit.localRotation;
     }
--- FYP_Nautilus/Assets/Scripts/Overworld/In-Game Menu Scripts/MapMenuManager.cs
+++ FYP_Nautilus/Assets/Scripts/Overworld/In-Game Menu Scripts/MapMenuManager.cs
@@ -16,12 +16,14 @@ public class MapMenuManager : MonoBehaviour
     //UI element
 
     bool inMenu;
-    
+    int menuLayer; // 0 is base, when open one submenu there, plus one. Vice Versa
+
     public void openMenu()
     {
+        menuLayer = 0;
         inMenu = true;
-        for(int i = 0; i < playerInfo.areas.Length; i++)
-        {            
+        for (int i = 0; i < playerInfo.areas.Length; i++)
+        {
             if (playerInfo.areas[i].unlocked)
             {
                 //area is unlocked -> enable the button
@@ -32,21 +34,43 @@ public class MapMenuManager : MonoBehaviour
         }
     }
 
-    void Update()
+    private void Update()
     {
-
+        if (inMenu)
+        {
+            if (Input.GetButtonDown("Cancel"))
+            {
+                if (menuLayer == 0)
+                {
+                    closeMenu();
+                }
+                else if (menuLayer == 1)
+                {
+                    closeArea();
+                }
+            }
+        }
     }
     public void openArea(int mapIndex)
     {
+        ++menuLayer;
         areaMapHolder.GetChild(mapIndex).gameObject.SetActive(true);
-        for(int i = 0; i < playerInfo.areas[mapIndex].areaPoints.Length; i++)
+        for (int i = 0; i < playerInfo.areas[mapIndex].areaPoints.Length; i++)
         {
-            if(playerInfo.areas[mapIndex].areaPoints[i].unlocked)
+            if (playerInfo.areas[mapIndex].areaPoints[i].unlocked)
             {
-                GameObject button = areaMapHolder.GetChild(mapIndex).GetChild(0).GetChild(i).gameObject;                
+                GameObject button = areaMapHolder.GetChild(mapIndex).GetChild(0).GetChild(i).gameObject;
                 button.SetActive(true);
                 button.GetComponent<MapMenuPointButton>().initButton(transferManager, menuManager, this, playerInfo.areas[mapIndex].areaPoints[i]);
             }
+        }
+    }
+    public void closeArea()
+    {
+        --menuLayer;
+        foreach(Transform obj in areaMapHolder)
+        {
+            obj.gameObject.SetActive(false);
         }
     }
     public void closeMenu()
